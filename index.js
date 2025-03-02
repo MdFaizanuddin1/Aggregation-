110c1d9b2a894f7553d61edaa1e254aebf50292e
Md Faizanuddin <120786015+MdFaizanuddin1@users.noreply.github.com>
2025-03-02 15:24:39 +0530

    count how many users have "ad" as the second tag in their list of tags

--- index.js
+++ index.js
@@ -239,3 +239,16 @@
 //     }
 //   }
 // ]
+
+// count how many users have "ad" as the second tag in their list of tags
+
+// [
+//   {
+//     $match: {
+//       "tags.1" : "ad"
+//     }
+//   },
+//   {
+//     $count: 'secondTagAsAd'
+//   }
+// ]
