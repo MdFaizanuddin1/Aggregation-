230b01e1f21a639e2f305c5d37a5a5815c492058
Md Faizanuddin <120786015+MdFaizanuddin1@users.noreply.github.com>
2025-03-02 15:00:51 +0530

    how many users have a phone number starting with '+1 (940)'

--- index.js
+++ index.js
@@ -187,3 +187,16 @@
 //     },
 //   },
 // ]
+
+// how many users have a phone number starting with '+1 (940)' ?
+
+// [
+//   {
+//     $match: {
+//       "company.phone": /\+1\s?\(940\)/
+//     }
+//   },
+//   {
+//     $count: 'userWithSpecialPhone'
+// 	},
+// ]
