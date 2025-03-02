b24d916217b8a1b73a624f398e021c290e0f195c
Md Faizanuddin <120786015+MdFaizanuddin1@users.noreply.github.com>
2025-03-02 16:08:30 +0530

    lookup implimentation

--- index.js
+++ index.js
@@ -282,3 +282,24 @@
 //     },
 //   },
 // ]
+
+// lookup implimentation 
+
+// [
+//   {
+//     $lookup: {
+//       from: "authors",
+//       localField: "author_id",
+//       foreignField: "_id",
+//       as: "author_details"
+//     }
+//   },
+//   {
+//     $addFields: {
+//       author_details: {
+//         // $first : "$author_details"
+//         $arrayElemAt :["$author_details",0]
+//       },
+//     }
+//   }
+// ]
