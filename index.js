3bb9e2e231d0b13e61b6d4542519b17f92650dbb
Md Faizanuddin <120786015+MdFaizanuddin1@users.noreply.github.com>
2025-03-02 15:21:07 +0530

    how many users have "ad" as the second tag in their list of tags

--- index.js
+++ index.js
@@ -229,3 +229,13 @@
 //     }
 //   }
 // ]
+
+// how many users have "ad" as the second tag in their list of tags
+
+// [
+//   {
+//     $match: {
+//       "tags.1" : "ad"
+//     }
+//   }
+// ]
